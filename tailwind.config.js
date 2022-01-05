c6b694417573f77d5d7ce09ee28a60c53ab06488
Thomas-Blakeney <blaumc@icloud.com>
2022-01-05 17:34:25 +0100

    feat: added tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   mode: "jit",
   content: ['src/pages/**/*.{js,jsx}', 'src/components/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#00FFE8',
+        'secondary': '#E48632',
+        'bg-primary': '#111111',
+      }
+    },
   },
   plugins: [],
 }
