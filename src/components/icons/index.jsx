fcb9116fd3b7444b79871b812b1af77006638dba
Thomas-Blakeney <blaumc@icloud.com>
2022-01-08 16:16:58 +0100

    feat: added index for icons

---
+++ src/components/icons/index.jsx
@@ -0,0 +1,13 @@
+import MainLogo from "./MainLogo";
+
+import CommLinksIcon from "./CommLinksIcon";
+import FleetIcon from "./FleetIcon";
+import GameplaysIcon from "./GameplaysIcon";
+import MemberIcon from "./MemberIcon";
+import PartnerIcon from "./PartnerIcon";
+import RecruitingIcon from "./RecruitingIcon";
+import ShipExkursIcon from "./ShipExkursIcon";
+import VerseExkursIcon from "./VerseExkursIcon";
+
+
+export { MainLogo, CommLinksIcon, FleetIcon, GameplaysIcon, MemberIcon, PartnerIcon, RecruitingIcon, ShipExkursIcon, VerseExkursIcon };
