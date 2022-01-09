907b49a107c35b509cd20dd0ad9c41420dd58087
Thomas-Blakeney <blaumc@icloud.com>
2022-01-09 23:51:06 +0100

    fix: fixed favicons

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -7,13 +7,63 @@ class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest.json" />
 
-          <meta name="msapplication-TileColor" content="#00ffe8" />
+          <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
 
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+          <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
         </Head>
         <body>
           <Main />
