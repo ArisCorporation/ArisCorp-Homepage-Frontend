dbf9c3c236f4d66ac7f5c7d3c47c9b6ae5e74729
Thomas-Blakeney <blaumc@icloud.com>
2022-01-09 21:28:20 +0100

    feat: added pwa and sample icons

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withPWA = require('next-pwa')
-
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA ({
   reactStrictMode: true,
   pwa: {
-    dest: 'public'
-  }
+    dest: 'public',
+    runtimeCaching,
+  },
 })
