47a5481e91070ccc0e2c86a5422b6bcc773903ae
Thomas-Blakeney <blaumc@icloud.com>
2022-01-10 02:30:08 +0100

    feat: herosection responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,11 +5,11 @@ const HeroSection = () => {
     <div className="relative">
       <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
         <div className="relative items-center justify-center">
-          <h1 className="flex items-center justify-center text-xl font-bold text-white mb-[-50px]">
+          <h1 className="flex items-center justify-center text-lg font-bold text-white mt-20 mb-[-40px] sm:mt-10 sm:mb-[-50px] sm:text-2xl md:mt-10 md:mb-[-30px] md:text-4xl lg:mt-0 lg:mb-0 lg:text-5xl xl:mb-10 xl:text-6xl">
             Willkommen
             <span className="ml-2 tracking-widest text-secondary">bei der</span>
           </h1>
-          <div className="flex items-center justify-center scale-50">
+          <div className="flex items-center justify-center scale-[.4] sm:scale-50 md:scale-[.6] lg:scale-75 xl:scale-100">
             <Image
               src="https://cms.ariscorp.de/assets/62eb0e48-6a0e-432f-b90d-fbd6aca6eaac"
               alt="ArisCorp Banner"
