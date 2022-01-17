2b3a12b2cfd047598ed31ce78b305ec039d16ca6
Thomas-Blakeney <blaumc@icloud.com>
2022-01-17 01:40:54 +0100

    test

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -7,7 +7,7 @@ import ArisCorpCharta from "./AboutSection/ArisCorpCharta";
 const AboutSection = ({ aboutData, historyData, manifestData, chartaData }) => (
   <Tabs>
     <TabList>
-      <h1>ÜBER</h1>
+      <h1>ÜBERRR</h1>
       <hr />
       <Tab><h1 className="text-base sm:text-lg md:text-xl lg:text-3xl xl:text-4xl">DIE ARISCORP</h1></Tab>
       <Tab><h1 className="text-base sm:text-lg md:text-xl lg:text-3xl xl:text-4xl">HISTORY</h1></Tab>
