1d00dda1cd8ebf6c7cd536b738f2c576ebf9e55a
Thomas-Blakeney <blaumc@icloud.com>
2022-01-17 01:44:37 +0100

    fix: fixed trailer-overlay

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
   return (
     <div className="relative mb-32 drop-shadow-2xl">
       <div className="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
-        
+      <div className="absolute w-full h-full bg-black opacity-60"></div>
         <div className="relative items-center justify-center">
           <h1 className="flex items-center justify-center text-lg font-bold text-white mt-20 mb-[-40px] sm:mt-10 sm:mb-[-50px] sm:text-2xl md:mt-10 md:mb-[-30px] md:text-4xl lg:mt-0 lg:mb-0 lg:text-5xl xl:mb-10 xl:text-6xl">
             Willkommen
@@ -30,7 +30,7 @@ const HeroSection = () => {
           className="w-full h-full"
         >
           <source
-            src="https://cms.ariscorp.de/assets/2fe8455b-d879-4703-b81f-2c9485794d93"
+            src="/assets/ArisCorp-Trailer.mp4"
             type='video/mp4'
           />
         </video>
