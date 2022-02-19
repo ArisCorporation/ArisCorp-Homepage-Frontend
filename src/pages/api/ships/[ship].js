4a894fc4f85841509cabfd2f9afb51526bcd9c22
Thomas-Blakeney <blaumc@icloud.com>
2022-02-19 23:31:36 +0100

    feat(api): added shipexkurs and items api -> e.g. /api/ships

---
+++ src/pages/api/ships/[ship].js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default async (req, res) => {
+  const { ship } = req.query
+  const url = `https://raw.githubusercontent.com/richardthombs/scunpacked/master/api/dist/json/v2/ships/${ship}.json`
+
+  await axios
+    .get(url)
+    .then(({ data }) => {
+      res.status(200).json({ data })
+    })
+    .catch(({ err }) => {
+      res.status(400).json({ err })
+    })
+}
