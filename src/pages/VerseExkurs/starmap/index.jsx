2e09ff10c11c9e5f5346a1af8e08140210b3620a
thomas-blakeney <blaumc@icloud.com>
2022-03-18 00:40:13 +0100

    fix(ve): fixed starmap layout

--- src/pages/VerseExkurs/starmap/index.jsx
+++ src/pages/VerseExkurs/starmap/index.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import ReactTooltip from 'react-tooltip'
-import Layout from '../../layout'
+import Layout from 'pages/VerseExkurs/layout'
 import { useState } from 'react'
 import Starmap from 'components/VerseExkursStarmap'
 import { Tab } from '@headlessui/react'
