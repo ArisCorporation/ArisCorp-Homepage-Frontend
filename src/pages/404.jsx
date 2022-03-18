4269f52e2f4f1675ec9f9c9b6193506330b5904f
thomas-blakeney <blaumc@icloud.com>
2022-03-18 15:21:30 +0100

    feat(error-pages): added basic 404 page

---
+++ src/pages/404.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+export default function Error404() {
+  const router = useRouter()
+
+  return (
+    <div className="flex flex-wrap items-center justify-center">
+      <div className="mx-auto">
+        <div className="pt-16 mx-auto">
+          <h1 className="uppercase text-primary">Status Code: 404</h1>
+          <h2 className="uppercase text-secondary">Lost in Verse</h2>
+        </div>
+        <div className="pt-12 mx-auto text-center">
+          <div
+            onClick={() => router.back()}
+            className="p-2 text-gray-300 transition-all duration-150 rounded-full hover:cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:no-underline bg-secondary"
+          >
+            Take me Back on Course!
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
