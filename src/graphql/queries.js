6668c28410640e3a5f8ddabcbe7de6a58c7ed6e5
thomas-blakeney <blaumc@icloud.com>
2022-04-24 16:45:05 +0200

    fix: fixed empty search in comm-link

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -178,7 +178,7 @@ export const GET_GAMEPLAYS = gql`
 
 // COMM-LINK QUERYS
 export const GET_COMM_LINKS = gql`
-  query GetCommLinks($queryChannel: String!, $squery: String!) {
+  query GetCommLinks($queryChannel: String!, $squery: String = "") {
     comm_links(
       #filter: { comm_link_channel: { channel: { _contains: $queryChannel} } }
       filter: {
