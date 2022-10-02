dcc8c6f19f0022f67b71397e983ff4bdf962fabf
GruberL <admin@firecave.de>
2022-10-02 13:47:19 +0200

    refactor(Weapon Page): Changed Path and Added Mobile Compatability

--- src/pages/VerseExkurs/waffen/[weapon].jsx
+++ src/pages/VerseExkurs/waffen/[weapon].jsx
@@ -57,14 +57,14 @@ export default function SpectrumArticlePage() {
     <div className="items-center max-w-6xl pt-10 mx-auto print:pt-5">
       <div>
         <div className="flex items-center justify-center align-center">
-          <h1 className="text-6xl italic">
+          <h1 className="text-4xl italic md:text-5xl lg:text-6xl">
             {' '}
             <span>{data1.waffen_name}</span>{' '}
             <span className="text-secondary">
               {data1.waffen_klasse.waffenklasse}
             </span>{' '}
           </h1>
-          <div className="relative h-48 ml-auto -my-10 aspect-square">
+          <div className="relative ml-auto -my-10 xs:h-32 h-28 xmlnsXlink sm:h-40 md:h-48 aspect-square">
             <Image
               src={
                 'https://cms.ariscorp.de/assets/' +
@@ -83,16 +83,15 @@ export default function SpectrumArticlePage() {
         </div>
         <hr />
       </div>
-      <div className="flex w-full space-x-2">
-        <BasicPanel className="w-3/5" width="3/5" bottom>
-          <div className="relative rounded-3xl overflow-hidden w-full aspect-[760/430]">
+      <div className="w-full space-y-2 xl:space-x-2 lg:flex lg:flex-wrap xl:flex-nowrap">
+        <BasicPanel className="w-full xl:w-3/5" bottom>
+          <div className="relative rounded-3xl overflow-hidden w-full aspect-[21/9] xl:aspect-[760/430]">
             <Image
               src={'https://cms.ariscorp.de/assets/' + data1.waffen_bild.id}
               alt={'Bild von ' + data1.waffen_name}
               layout="fill"
               objectFit="cover"
               placeholder="blur"
-              className=""
               blurDataURL={
                 'https://cms.ariscorp.de/assets/' +
                 data1.waffen_bild.id +
@@ -101,14 +100,16 @@ export default function SpectrumArticlePage() {
             />
           </div>
         </BasicPanel>
-        <BasicPanel className="w-2/5" width="2/5">
-          <div className="w-full h-full pb-2 italic uppercase">
-            <div className="">
-              <p className="pt-2 pl-3 m-0 text-secondary">Spezifikationen</p>
-            </div>
-            <table className="w-full ml-5 text-sm">
-              <tr className="">
-                <th className="text-left">Klassifizierung:</th>
+        <BasicPanel className="w-full xl:w-2/5">
+          <div className="w-full h-full pb-2 text-xs italic uppercase xs:text-sm">
+            <table className="w-full ml-5 table-auto">
+              <div>
+                <p className="pt-2 m-0 -ml-2 text-sm xs:text-base text-secondary">
+                  Spezifikationen
+                </p>
+              </div>
+              <tr>
+                <th className="pr-2 text-left">Klassifizierung:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_klasse.waffenklasse != null
                     ? data1.waffen_klasse.waffenklasse +
@@ -118,41 +119,43 @@ export default function SpectrumArticlePage() {
                     : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Hersteller:</th>
+              <tr>
+                <th className="pr-2 text-left">Hersteller:</th>
                 <td className="text-left text-primary">
                   {data1.waffenhersteller.firmen_name != null
                     ? data1.waffenhersteller.firmen_name
                     : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Gewicht:</th>
+              <tr>
+                <th className="pr-2 text-left">Gewicht:</th>
                 <td className="text-left text-primary">
                   {data1.waffengewicht != null
                     ? data1.waffengewicht + ' KG'
                     : 'N/A'}
                 </td>
               </tr>
-            </table>
-            <hr className="w-[85%] ml-4 relative bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
-            <table className="w-full ml-5 text-sm">
-              <tr className="">
-                <th className="text-left">Schadenstyp:</th>
-                <td className="pr-4 text-left break-words text-primary">
+              <tr>
+                <td colSpan="2">
+                  <hr className="relative w-[85%] mt-3 sm:mt-5 sm:mb-4 mb-2 -ml-1 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
+                </td>
+              </tr>
+              <tr>
+                <th className="pr-2 text-left">Schadenstyp:</th>
+                <td className="text-left text-primary">
                   {data1.wafffen_schadenstyp.schadenstyp != null
                     ? data1.wafffen_schadenstyp.schadenstyp
                     : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Feuermodi:</th>
-                <td className="pr-4 text-left break-words text-primary">
+              <tr>
+                <th className="pr-2 text-left">Feuermodi:</th>
+                <td className="text-left break-words text-primary">
                   {data2[0] != null ? data2arr : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Feuerrate:</th>
+              <tr>
+                <th className="pr-2 text-left">Feuerrate:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_feuerrate_einzel ||
                   data1.waffen_feuerrate_salve ||
@@ -173,28 +176,32 @@ export default function SpectrumArticlePage() {
                     : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="text-left">Mündungs Geschwindigkeit:</th>
+              <tr>
+                <th className="pr-2 text-left">Mündungs Geschwindigkeit:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_muendungs_geschwindigkeit != null
                     ? data1.waffen_muendungs_geschwindigkeit
                     : 'N/A'}
                 </td>
               </tr>
-            </table>
-            <hr className="w-[85%] ml-4 relative mb-0 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
-            <div className="">
-              <p className="pt-0 pl-3 m-0 text-secondary">Attachments</p>
-            </div>
-            <table className="w-full ml-5 text-sm">
-              <tr className="">
-                <th className="text-left">Magazine:</th>
-                <td className="pr-4 text-left break-words text-primary">
+              <tr>
+                <td colSpan="2">
+                  <hr className="relative w-[85%] mt-3 -ml-1 mb-0 sm:mt-5 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
+                </td>
+              </tr>
+              <div>
+                <p className="pt-0 m-0 -ml-2 text-sm xs:text-base text-secondary">
+                  Attachments
+                </p>
+              </div>
+              <tr>
+                <th className="pr-2 text-left">Magazine:</th>
+                <td className="text-left break-words text-primary">
                   {data1.waffen_magazin != null ? data1.waffen_magazin : 'N/A'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Visierung:</th>
+              <tr>
+                <th className="pr-2 text-left">Visierung:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_visier != null
                     ? data1.waffen_visier
@@ -204,8 +211,8 @@ export default function SpectrumArticlePage() {
                       ')'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Lauf:</th>
+              <tr>
+                <th className="pr-2 text-left">Lauf:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_lauf != null
                     ? data1.waffengewicht + ' KG'
@@ -215,8 +222,8 @@ export default function SpectrumArticlePage() {
                       ')'}
                 </td>
               </tr>
-              <tr className="">
-                <th className="pr-5 text-left">Unterlauf:</th>
+              <tr>
+                <th className="pr-2 text-left">Unterlauf:</th>
                 <td className="text-left text-primary">
                   {data1.waffen_unterlauf != null
                     ? data1.waffengewicht + ' KG'
@@ -270,11 +277,11 @@ export default function SpectrumArticlePage() {
           <Tab.Panels className={'px-4 xl:px-0 pt-5'}>
             <BasicPanel>
               <Tab.Panel>
-                <div className="flex">
-                  <div className="w-5/12 pb-5 ml-5 italic uppercase">
+                <div className="flex flex-wrap text-xs lg:flex-nowrap sm:text-sm md:text-base">
+                  <div className="pb-5 ml-5 italic uppercase lg:w-5/12">
                     <p className="text-secondary">Statistiken</p>
-                    <table className="">
-                      <tr className="">
+                    <table>
+                      <tr>
                         <th className="text-left">Maximale Reichweite:</th>
                         <td className="text-left text-primary">
                           {data1.waffen_maximale_reichweite != null
@@ -282,7 +289,7 @@ export default function SpectrumArticlePage() {
                             : 'N/A'}
                         </td>
                       </tr>
-                      <tr className="">
+                      <tr>
                         <th className="pr-5 text-left">
                           Effektive Reichweite:
                         </th>
@@ -333,11 +340,13 @@ export default function SpectrumArticlePage() {
                     </table>
                     <hr className="w-[98%] relative mt-1 mb-2 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
                   </div>
-                  <div className="w-7/12 pb-5 pl-8">
-                    <p className="text-secondary">Beschreibung</p>
+                  <div className="pb-5 pl-8 pr-8 lg:pr-4 xl:pr-0 lg:w-7/12">
+                    <p className="italic uppercase text-secondary">
+                      Beschreibung
+                    </p>
                     <ReactMarkdown
                       rehypePlugins={[rehypeRaw]}
-                      className="prose prose-td:align-middle prose-invert xl:max-w-[90%] text-lg"
+                      className="prose prose-td:align-middle prose-invert xl:max-w-[90%] md:text-lg"
                     >
                       {data1.waffen_beschreibung}
                     </ReactMarkdown>
@@ -353,7 +362,7 @@ export default function SpectrumArticlePage() {
               <Tab.Panel>
                 <ReactMarkdown
                   rehypePlugins={[rehypeRaw]}
-                  className="mx-auto prose prose-td:align-middle prose-invert xl:max-w-[90%]"
+                  className="mx-auto prose prose-td:align-middle prose-invert xl:max-w-[90%] md:text-"
                 >
                   c
                 </ReactMarkdown>
