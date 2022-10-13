71fb8ef90e1af7fe8bc78c0a3c23ccddf7b5caa9
GruberL <admin@firecave.de>
2022-10-13 21:59:46 +0200

    fix(VerseExkurs Timeline): Fixed Timeline

--- src/components/VerseExkursTimeline.jsx
+++ src/components/VerseExkursTimeline.jsx
@@ -2,87 +2,16 @@
 import dynamic from 'next/dynamic'
 const Timeline = dynamic(() => import('timelinejs-react'), { ssr: false })
 
-const events = [
-  {
-    start_date: {
-      year: 2021,
-      month: 6,
-      day: 5,
-    },
-    media: {
-      url: 'https://picsum.photos/200/300',
-      thumbnail: 'https://picsum.photos/200/300',
-      caption: 'google',
-      link: 'https://google.de',
-    },
-    unique_id: '1',
-    text: {
-      headline: 'Event1',
-      text: '',
-    },
-    group: 'Catégorie1',
-  },
-  {
-    start_date: {
-      year: 2021,
-      month: 8,
-      day: 4,
-    },
-    media: {
-      url: 'https://picsum.photos/200/300',
-      thumbnail: 'https://picsum.photos/200/300',
-      caption: '',
-      link: '',
-    },
-    end_date: {
-      year: 2021,
-      month: 9,
-      day: 5,
-    },
-    unique_id: '2',
-    text: {
-      headline: 'Event2',
-      text: '',
-    },
-    group: 'Catégorie1',
-    background: {},
-  },
-  {
-    start_date: {
-      year: 2021,
-      month: 8,
-      day: 4,
-    },
-    media: {
-      url: 'https://picsum.photos/200/300',
-      thumbnail: 'https://picsum.photos/200/300',
-      caption: '',
-      link: '',
-    },
-    end_date: {
-      year: 2021,
-      month: 8,
-      day: 5,
-    },
-    unique_id: '3',
-    text: {
-      headline: 'Event3',
-      text: '',
-    },
-    group: 'Catégorie1',
-    background: {},
-  },
-]
-
-const options = {
-}
+// const options = {
+//   timenav_height: 300
+// }
 const TimelineComponent = ({ events, data }) => (
   <>
-      {/* <Timeline
-        target={<div className="timeline" style={{ height: 600 }} />}
+      <Timeline
+        target={<div className="timeline" style={{ height: 800 }} />}
         events={events}
-        options={options}
-      /> */}
+        options={{start_at_end: true}}
+      />
       <link rel="stylesheet" href="https://cdn.knightlab.com/libs/timeline3/latest/css/themes/timeline.theme.dark.css" />
       <style>{`
         .tl-slider-background{
