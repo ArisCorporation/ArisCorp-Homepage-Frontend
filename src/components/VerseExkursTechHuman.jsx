57a4da94122b8421406cedc146cd5bd23fc12034
GruberL <admin@firecave.de>
2022-10-19 21:47:15 +0200

    fix(Tech Personel): fixed weapon link on svg

--- src/components/VerseExkursTechHuman.jsx
+++ src/components/VerseExkursTechHuman.jsx
@@ -140,7 +140,7 @@ const TechHuman = () => {
         className="transition-colors duration-200 group hover:duration-300 hover:cursor-pointer"
         onMouseEnter={() => setSelectedTech('weaponexkurs')}
         onMouseLeave={() => setSelectedTech(undefined)}
-        onClick={() => router.push('/VerseExkurs/weaponexkurs')}
+        onClick={() => router.push('/VerseExkurs/waffen')}
       >
         <path
           id="weapons1"
