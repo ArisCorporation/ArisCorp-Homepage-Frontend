f0d80c7cfb6ee052726ab6d6c22b7b7568a74a55
GruberL <admin@firecave.de>
2022-10-20 17:37:45 +0200

    refactor(VerseExkurs - Weapon Site): Added Mobile compatibillity and added ChildClassName to BasicPanel

--- src/pages/VerseExkurs/waffen/[weapon].jsx
+++ src/pages/VerseExkurs/waffen/[weapon].jsx
@@ -1,7 +1,7 @@
 import Layout from 'pages/VerseExkurs/layout'
 import { useEffect, useState } from 'react'
 import { SquareLoader } from 'react-spinners'
-import Image from 'next/image'
+import Image from 'next/future/image'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import Router, { useRouter } from 'next/router'
@@ -25,7 +25,6 @@ export default function SpectrumArticlePage() {
     }
   }, [urlquery])
 
-
   const { loading, error, data } = useQuery(GET_VERSEEXKURS_WEAPON, {
     variables: { Weapon: Weapon },
   })
@@ -47,7 +46,7 @@ export default function SpectrumArticlePage() {
       )
     )
   )
-  const data2arr = []
+  let data2arr = []
   data2.map((data) => data2arr.push(data.waffen_feuermodi_id.feuermodus))
   data2arr = data2arr.join(' / ')
   // console.log(data2arr);
@@ -56,21 +55,28 @@ export default function SpectrumArticlePage() {
     <div className="items-center max-w-6xl pt-10 mx-auto print:pt-5">
       <div>
         <div className="flex items-center justify-center align-center">
-          <h1 className="text-4xl italic md:text-5xl lg:text-6xl">
+          <h1 className="text-2xl italic xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
             {' '}
             <span>{data1.waffen_name}</span>{' '}
             <span className="text-secondary">
               {data1.waffen_klasse.waffenklasse}
             </span>{' '}
           </h1>
-          <div className="relative ml-auto -my-10 hover:cursor-pointer xs:h-32 h-28 xmlnsXlink xxs:h-24 sm:h-40 md:h-48 aspect-square" onClick={() => router.push("/VerseExkurs/firmen/" + data1.waffenhersteller.firmen_name)}>
+          <div
+            className="relative ml-auto -my-10 hover:cursor-pointer xs:h-32 h-28 xmlnsXlink xxs:h-24 sm:h-40 md:h-48 aspect-square"
+            onClick={() =>
+              router.push(
+                '/VerseExkurs/firmen/' + data1.waffenhersteller.firmen_name
+              )
+            }
+          >
             <Image
               src={
                 'https://cms.ariscorp.de/assets/' +
                 data1.waffenhersteller.firmen_trans_logo.id
               }
               alt={'Logo von ' + data1.waffenhersteller.firmen_name}
-              layout="fill"
+              fill
               placeholder="blur"
               blurDataURL={
                 'https://cms.ariscorp.de/assets/' +
@@ -82,14 +88,17 @@ export default function SpectrumArticlePage() {
         </div>
         <hr />
       </div>
-      <div className="w-full space-y-2 xl:space-x-2 lg:flex lg:flex-wrap xl:flex-nowrap">
-        <BasicPanel className="w-full xl:w-3/5" bottom>
-          <div className="relative rounded-3xl overflow-hidden w-full aspect-[21/9] xl:aspect-[760/430]">
+      <div className="w-full space-y-2 xl:space-y-0 xl:space-x-2 lg:flex lg:flex-wrap xl:flex-nowrap">
+        <div className="w-full aspect-[21/9] xl:aspect-auto xl:w-3/5">
+          <BasicPanel
+            className={'h-full'}
+            childClassName={'h-full overflow-hidden'}
+          >
             <Image
               src={'https://cms.ariscorp.de/assets/' + data1.waffen_bild.id}
               alt={'Bild von ' + data1.waffen_name}
-              layout="fill"
-              objectFit="cover"
+              fill
+              className="object-cover"
               placeholder="blur"
               blurDataURL={
                 'https://cms.ariscorp.de/assets/' +
@@ -97,144 +106,151 @@ export default function SpectrumArticlePage() {
                 '?width=16&quality=1'
               }
             />
-          </div>
-        </BasicPanel>
-        <BasicPanel className="w-full xl:w-2/5">
-          <div className="w-full h-full pb-2 text-xs italic uppercase xs:text-sm">
-            <table className="w-full ml-5 table-auto">
-              <div>
-                <p className="pt-2 m-0 -ml-2 text-sm xs:text-base text-secondary">
-                  Spezifikationen
-                </p>
-              </div>
-              <tr>
-                <th className="pr-2 text-left">Klassifizierung:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_klasse.waffenklasse != null
-                    ? data1.waffen_klasse.waffenklasse +
-                      ' (S' +
-                      data1.waffen_klasse.waffenklassensize.waffensize +
-                      ')'
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Hersteller:</th>
-                <td className="text-left text-primary">
-                  {data1.waffenhersteller.firmen_name != null
-                    ? data1.waffenhersteller.firmen_name
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Gewicht:</th>
-                <td className="text-left text-primary">
-                  {data1.waffengewicht != null
-                    ? data1.waffengewicht + ' KG'
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="2">
-                  <hr className="relative w-[85%] mt-3 sm:mt-5 sm:mb-4 mb-2 -ml-1 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Schadenstyp:</th>
-                <td className="text-left text-primary">
-                  {data1.wafffen_schadenstyp.schadenstyp != null
-                    ? data1.wafffen_schadenstyp.schadenstyp
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Feuermodi:</th>
-                <td className="text-left break-words text-primary">
-                  {data2[0] != null ? data2arr : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Feuerrate:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_feuerrate_einzel ||
-                  data1.waffen_feuerrate_salve ||
-                  data1.waffen_feuerrate_vollauto ||
-                  data1.waffen_feuerrate_aufgeladen != null
-                    ? (data1.waffen_feuerrate_einzel != null
-                        ? data1.waffen_feuerrate_einzel + '/Einzel  '
-                        : '') +
-                      (data1.waffen_feuerrate_salve != null
-                        ? data1.waffen_feuerrate_salve + '/Salve  '
-                        : '') +
-                      (data1.waffen_feuerrate_vollauto != null
-                        ? data1.waffen_feuerrate_vollauto + '/Gebündelt  '
-                        : '') +
-                      (data1.waffen_feuerrate_aufgeladen != null
-                        ? data1.waffen_feuerrate_aufgeladen + '/Aufgeladen  '
-                        : '')
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Mündungs Geschwindigkeit:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_muendungs_geschwindigkeit != null
-                    ? data1.waffen_muendungs_geschwindigkeit
-                    : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <td colSpan="2">
-                  <hr className="relative w-[85%] mt-3 -ml-1 mb-0 sm:mt-5 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
-                </td>
-              </tr>
-              <div>
-                <p className="pt-0 m-0 -ml-2 text-sm xs:text-base text-secondary">
-                  Attachments
-                </p>
-              </div>
-              <tr>
-                <th className="pr-2 text-left">Magazine:</th>
-                <td className="text-left break-words text-primary">
-                  {data1.waffen_magazin != null ? data1.waffen_magazin : 'N/A'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Visierung:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_visier != null
-                    ? data1.waffen_visier.visiername
-                    : 'Leer' +
-                      ' (S' +
-                      data1.waffen_klasse.waffenklassensize.waffensize +
-                      ')'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Lauf:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_lauf != null
-                    ? data1.waffengewicht + ' KG'
-                    : 'Leer' +
-                      ' (S' +
-                      data1.waffen_klasse.waffenklassensize.waffensize +
-                      ')'}
-                </td>
-              </tr>
-              <tr>
-                <th className="pr-2 text-left">Unterlauf:</th>
-                <td className="text-left text-primary">
-                  {data1.waffen_unterlauf != null
-                    ? data1.waffengewicht + ' KG'
-                    : 'Leer' +
-                      ' (S' +
-                      data1.waffen_klasse.waffenklassensize.waffensize +
-                      ')'}
-                </td>
-              </tr>
-            </table>
-          </div>
-        </BasicPanel>
+          </BasicPanel>
+        </div>
+        <div className="w-full xl:w-2/5">
+          <BasicPanel
+            className={'h-full'}
+            childClassName={'h-full overflow-hidden'}
+          >
+            <div className="w-full h-full px-5 pb-2 text-xs italic uppercase xs:text-sm">
+              <table className="w-full space-y-10 table-auto">
+                <div>
+                  <p className="pt-2 m-0 -ml-2 text-sm xs:text-base text-secondary">
+                    Spezifikationen
+                  </p>
+                </div>
+                <tr>
+                  <th className="pr-2 text-left">Klassifizierung:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_klasse.waffenklasse != null
+                      ? data1.waffen_klasse.waffenklasse +
+                        ' (S' +
+                        data1.waffen_klasse.waffenklassensize.waffensize +
+                        ')'
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Hersteller:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffenhersteller.firmen_name != null
+                      ? data1.waffenhersteller.firmen_name
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Gewicht:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffengewicht != null
+                      ? data1.waffengewicht + ' KG'
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2">
+                    <hr className="relative w-[85%] mt-3 sm:mt-5 sm:mb-4 mb-2 -ml-1 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Schadenstyp:</th>
+                  <td className="text-left text-primary">
+                    {data1.wafffen_schadenstyp.schadenstyp != null
+                      ? data1.wafffen_schadenstyp.schadenstyp
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Feuermodi:</th>
+                  <td className="text-left break-words text-primary">
+                    {data2[0] != null ? data2arr : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Feuerrate:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_feuerrate_einzel ||
+                    data1.waffen_feuerrate_salve ||
+                    data1.waffen_feuerrate_vollauto ||
+                    data1.waffen_feuerrate_aufgeladen != null
+                      ? (data1.waffen_feuerrate_einzel != null
+                          ? data1.waffen_feuerrate_einzel + '/Einzel  '
+                          : '') +
+                        (data1.waffen_feuerrate_salve != null
+                          ? data1.waffen_feuerrate_salve + '/Salve  '
+                          : '') +
+                        (data1.waffen_feuerrate_vollauto != null
+                          ? data1.waffen_feuerrate_vollauto + '/Gebündelt  '
+                          : '') +
+                        (data1.waffen_feuerrate_aufgeladen != null
+                          ? data1.waffen_feuerrate_aufgeladen + '/Aufgeladen  '
+                          : '')
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Mündungs Geschwindigkeit:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_muendungs_geschwindigkeit != null
+                      ? data1.waffen_muendungs_geschwindigkeit
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan="2">
+                    <hr className="relative w-[85%] mt-3 -ml-1 mb-0 sm:mt-5 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
+                  </td>
+                </tr>
+                <div>
+                  <p className="pt-0 m-0 -ml-2 text-sm xs:text-base text-secondary">
+                    Attachments
+                  </p>
+                </div>
+                <tr>
+                  <th className="pr-2 text-left">Magazine:</th>
+                  <td className="text-left break-words text-primary">
+                    {data1.waffen_magazin != null
+                      ? data1.waffen_magazin
+                      : 'N/A'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Visierung:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_visier != null
+                      ? data1.waffen_visier.visiername
+                      : 'Leer' +
+                        ' (S' +
+                        data1.waffen_klasse.waffenklassensize.waffensize +
+                        ')'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Lauf:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_lauf != null
+                      ? data1.waffengewicht + ' KG'
+                      : 'Leer' +
+                        ' (S' +
+                        data1.waffen_klasse.waffenklassensize.waffensize +
+                        ')'}
+                  </td>
+                </tr>
+                <tr>
+                  <th className="pr-2 text-left">Unterlauf:</th>
+                  <td className="text-left text-primary">
+                    {data1.waffen_unterlauf != null
+                      ? data1.waffengewicht + ' KG'
+                      : 'Leer' +
+                        ' (S' +
+                        data1.waffen_klasse.waffenklassensize.waffensize +
+                        ')'}
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </BasicPanel>
+        </div>
       </div>
       <hr />
       <div>
@@ -276,8 +292,8 @@ export default function SpectrumArticlePage() {
           <Tab.Panels className={'px-4 xl:px-0 pt-5'}>
             <BasicPanel>
               <Tab.Panel>
-                <div className="flex flex-wrap text-xs xl:flex-nowrap sm:text-sm md:text-base">
-                  <div className="pb-5 ml-5 italic uppercase lg:w-5/12">
+                <div className="flex flex-wrap text-xs xl:space-x-2 xl:flex-nowrap sm:text-sm md:text-base">
+                  <div className="px-2 pb-5 mx-auto italic uppercase xs:mx-0 sm:px-8 lg:px-4 xl:pr-0 xl:pl-4 lg:w-5/12">
                     <p className="text-secondary">Statistiken</p>
                     <table>
                       <tr>
@@ -339,7 +355,7 @@ export default function SpectrumArticlePage() {
                     </table>
                     <hr className="w-[98%] relative mt-1 mb-2 bg-bg-secondary before:w-1 before:aspect-square before:absolute before:inline-block before:bg-primary after:w-1 after:right-0 after:aspect-square after:absolute after:inline-block after:bg-primary" />
                   </div>
-                  <div className="pb-5 pl-8 pr-8 lg:pr-4 xl:pr-0 lg:w-7/12">
+                  <div className="px-2 pb-5 sm:px-8 lg:px-4 xl:pr-0 xl:pl-4 xl:w-7/12">
                     <p className="italic uppercase text-secondary">
                       Beschreibung
                     </p>
