eae1e972ac430a47a247a6ffec897f740c7a84a3
GruberL <admin@firecave.de>
2022-10-21 18:38:43 +0200

    added onedayinhistory banner to timeline

--- src/pages/VerseExkurs/timeline.jsx
+++ src/pages/VerseExkurs/timeline.jsx
@@ -3,14 +3,15 @@ import { GET_VERSEEXKURS_TIMELINE } from 'graphql/queries'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import React from 'react'
 import client from 'apollo/clients'
 import TLComponent from 'components/VerseExkursTimeline'
+import Image from 'next/image'
 
 export async function getServerSideProps() {
   const { data } = await client.query({ query: GET_VERSEEXKURS_TIMELINE })
   const timelineEvents = []
-
 
   // console.log(data.timeline.event.length);
 
@@ -25,16 +26,16 @@ export async function getServerSideProps() {
     const banner =
       'https://cms.ariscorp.de/assets/21193739-402c-48fc-9a04-f6a8ca1537ea?width=1118&height=351'
     var group
-    
+
     if (object.category == 'ariscorp') {
       group = 'ArisCorp'
     } else if (object.category == 'verse') {
       group = 'Verse Timeline'
     } else if (object.category == 'one_day_in_history') {
       group = 'Ein Tag in der Geschichte'
-    } else if (object.category == 'firma'){
+    } else if (object.category == 'firma') {
       group = 'Firmengründungen'
-    } else if (object.category == 'epoche'){
+    } else if (object.category == 'epoche') {
       group = 'Epochen'
     } else {
       group = 'undefined'
@@ -141,6 +142,7 @@ export async function getServerSideProps() {
 }
 
 export default function TimelinePage({ data, events }) {
+  const { push } = useRouter()
   // {
   //   start_date: {
   //     year: 2021,
@@ -161,11 +163,33 @@ export default function TimelinePage({ data, events }) {
   //   group: 'Catégorie1',
   // }
 
-
-
   return (
     <div>
       <TLComponent data={data} events={events} />
+      <div className="mt-24 ml-2">
+        <p className="pl-6">
+          Klicke auf das Banner um auf die direkt Ansicht von {'"'}Ein Tag in
+          der Geschichte{'"'} zu gelangen.
+        </p>
+        <div
+          className="flex pl-4 flex-wrap hover:cursor-pointer h-full w-2/3 aspect-[1118/351]"
+          onClick={() => push('/VerseExkurs/onedayinhistory')}
+        >
+          <div className="relative w-full h-full">
+            <Image
+              src="https://cms.ariscorp.de/assets/c0a200fa-83d3-48f1-b6e4-905960cab504"
+              layout="fill"
+              alt="Spectrum Banner"
+              placeholder="blur"
+              blurDataURL="https://cms.ariscorp.de/assets/c0a200fa-83d3-48f1-b6e4-905960cab504?width=16&quality=1"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        <div className="">
+          <hr />
+        </div>
+      </div>
     </div>
   )
 }
