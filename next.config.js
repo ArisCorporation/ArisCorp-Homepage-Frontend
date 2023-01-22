d7cfb5a6755932e4163f0ff919e46a70a643de9e
lucas-gruber <blaumc@icloud.com>
2023-01-23 00:06:12 +0100

    feat: added x-robots-tag: noindex for ptu environment

--- next.config.js
+++ next.config.js
@@ -34,4 +34,20 @@ module.exports = withPWA(withConfig({
 
     return config
   },
+
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex',
+          },
+        ],
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 }))
