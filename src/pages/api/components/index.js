3a71806ee855bd04260de63c75f5524a864984c5
lucas-gruber <blaumc@icloud.com>
2023-03-08 18:10:51 +0100

    fix: fixed component manufacturer links

--- src/pages/api/components/index.js
+++ src/pages/api/components/index.js
@@ -77,7 +77,7 @@ async function formData() {
         return
       }
       const slug = string_to_slug(obj.stdItem.Name.toLowerCase())
-      const company = manufacturers.filter((e) => e.code == obj.manufacturer || e.firmen_name == obj?.stdItem?.Manufacturer.Name)[0]
+      const company = manufacturers.filter((e) => e.code?.toLowerCase() == obj.manufacturer?.toLowerCase() || e.firmen_name.toLowerCase() == obj?.stdItem?.Manufacturer?.Name.toLowerCase())[0]
 
       const component = {
         status: 'published',
