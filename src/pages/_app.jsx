d668ac8dacf7f308f5017b6d0acb52129d6ce390
lucas-gruber <blaumc@icloud.com>
2023-11-14 15:02:46 +0100

    feat: fix for registration process

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,16 +3,18 @@ import dynamic from 'next/dynamic'
 import { SessionProvider } from "next-auth/react"
 import client from 'apollo/clients'
 import { ApolloProvider } from '@apollo/client'
+import { useRouter } from 'next/router'
 
 const NextNProgress = dynamic(() => import('nextjs-progressbar'))
 
 function MyApp ({ Component, pageProps: { session, ...pageProps } }) {
   const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
 
   return getLayout(
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
-        <NextNProgress color="#00ffe8" height={2} />
+        {!router.pathname.startsWith("/registration") && (<NextNProgress color="#00ffe8" height={2} />)}
         <Component {...pageProps} />
       </SessionProvider>
     </ApolloProvider>
