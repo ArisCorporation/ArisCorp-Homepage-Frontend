244ece27cddf728ad79d8117065e44e06ccf896f
lucas-gruber <blaumc@icloud.com>
2023-11-20 20:50:57 +0100

    feat: added new icons to ams sidebar

--- src/components/internal/InternalSidebar.jsx
+++ src/components/internal/InternalSidebar.jsx
@@ -35,6 +35,8 @@ import { signOut, useSession } from 'next-auth/react'
 import Modal from 'components/modal'
 import DefaultButton from 'components/DefaultButton'
 import HangarIcon from 'components/icons/HangarIcon'
+import FleetIcon from 'components/icons/AMS-FleetIcon'
+import MeinProfilIcon from 'components/icons/MeinProfilIcon'
 
 export default function Sidebar ({ changes }) {
   const router = useRouter()
@@ -429,10 +431,10 @@ export default function Sidebar ({ changes }) {
                 <Link legacyBehavior href="/internal/fleet">
                   <a
                     className={
-                      'group-hover:text-[#e2e2e2] relative block py-[10px] pl-[30px] pr-[15px] text-lg whitespace-nowrap transition-all duration-500 ease-linear outline-0 outline-none decoration-transparent after:absolute after:top-[10%] after:left-0 after:w-[3px] h-[80%] after:bg-[#00ffe8] after:shadow-[2px_0_10px_rgba(36,86,130,.9)] after:transition-all after:rounded-r-sm after:duration-500 after:ease-linear after:h-[80%] after:text-transparent ' +
+                      'group-hover:text-[#e2e2e2] group-hover:fill-[#e2e2e2] relative block py-[10px] pl-[30px] pr-[15px] text-lg whitespace-nowrap transition-all duration-500 ease-linear outline-0 outline-none decoration-transparent after:absolute after:top-[10%] after:left-0 after:w-[3px] h-[80%] after:bg-[#00ffe8] after:shadow-[2px_0_10px_rgba(36,86,130,.9)] after:transition-all after:rounded-r-sm after:duration-500 after:ease-linear after:h-[80%] after:text-transparent ' +
                       (router.pathname.startsWith('/internal/fleet')
-                        ? 'after:block text-white '
-                        : 'after:hidden text-[#afafaf] ') +
+                        ? 'after:block text-white fill-white '
+                        : 'after:hidden text-[#afafaf] fill-[#afafaf] ') +
                       (!mobileView && sidebarCollapsed
                         ? 'py-[10px] pl-[25px] pr-[10px] text-2xl'
                         : '')
@@ -445,7 +447,7 @@ export default function Sidebar ({ changes }) {
                         (!mobileView && sidebarCollapsed ? 'h-[30px]' : '')
                       }
                     >
-                      <FaSpaceShuttle className="min-w-[30px] text-center relative antialiased inline-block" />
+                      <FleetIcon classNames="min-w-[30px] max-w-[30px] text-center relative antialiased inline-block" />
                       <span
                         className={
                           'ml-[5px] ' +
@@ -709,7 +711,7 @@ export default function Sidebar ({ changes }) {
                           (!mobileView && sidebarCollapsed ? 'h-[30px]' : '')
                         }
                       >
-                        <HangarIcon classes="fill-inherit" classNames="min-w-[30px] max-w-[30px] text-center relative antialiased inline-block" />
+                        <HangarIcon classNames="min-w-[30px] max-w-[30px] text-center relative antialiased inline-block" />
                         <span
                           className={
                             'ml-[5px] ' +
@@ -796,10 +798,10 @@ export default function Sidebar ({ changes }) {
                   <Link legacyBehavior href="/internal/profile">
                     <a
                       className={
-                        'group-hover:text-[#e2e2e2] relative block py-[10px] pl-[30px] pr-[15px] text-lg whitespace-nowrap transition-all duration-500 ease-linear outline-0 outline-none decoration-transparent after:absolute after:top-[10%] after:left-0 after:w-[3px] h-[80%] after:bg-[#00ffe8] after:shadow-[2px_0_10px_rgba(36,86,130,.9)] after:transition-all after:rounded-r-sm after:duration-500 after:ease-linear after:h-[80%] after:text-transparent ' +
+                        'group-hover:text-[#e2e2e2] group-hover:fill-[#e2e2e2] relative block py-[10px] pl-[30px] pr-[15px] text-lg whitespace-nowrap transition-all duration-500 ease-linear outline-0 outline-none decoration-transparent after:absolute after:top-[10%] after:left-0 after:w-[3px] h-[80%] after:bg-[#00ffe8] after:shadow-[2px_0_10px_rgba(36,86,130,.9)] after:transition-all after:rounded-r-sm after:duration-500 after:ease-linear after:h-[80%] after:text-transparent ' +
                         (router.pathname.startsWith('/internal/profile')
-                          ? 'after:block text-white '
-                          : 'after:hidden text-[#afafaf] ') +
+                          ? 'after:block text-white fill-white '
+                          : 'after:hidden text-[#afafaf] fill-[#afafaf] ') +
                         (!mobileView && sidebarCollapsed
                           ? 'py-[10px] pl-[25px] pr-[10px] text-2xl'
                           : '')
@@ -812,7 +814,7 @@ export default function Sidebar ({ changes }) {
                           (!mobileView && sidebarCollapsed ? 'h-[30px]' : '')
                         }
                       >
-                        <BiUserCircle className="min-w-[30px] text-center relative antialiased inline-block" />
+                        <MeinProfilIcon classNames="min-w-[30px] max-w-[30px] text-center relative antialiased inline-block" />
                         <span
                           className={
                             'ml-[5px] ' +
